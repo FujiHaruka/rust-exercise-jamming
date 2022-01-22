2ade3c2029e48f555f3559860ff80acc8adedbf4
FujiHaruka <fizzyjohn1427@gmail.com>
2022-01-22 11:49:12 +0900

    add unit test

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,30 @@ struct CliArgs {
     delimiter: String,
 }
 
+fn generate_jammed_string(text: &str, delimiter: &str) -> String {
+    let jammed = text
+        .split("")
+        .filter(|st| st.len() > 0)
+        .collect::<Vec<&str>>()
+        .join(&delimiter);
+    String::from(jammed)
+}
+
 fn main() {
     let args = CliArgs::parse();
 
     let text = args.text;
     let delimiter = args.delimiter;
 
-    let jammed = text
-        .split("")
-        .filter(|st| st.len() > 0)
-        .collect::<Vec<&str>>()
-        .join(&delimiter);
+    let jammed = generate_jammed_string(&text, &delimiter);
 
     println!("{}", jammed);
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn generate_jammed_string() {
+        assert_eq!(super::generate_jammed_string("abcdef", "/"), "a/b/c/d/e/f");
+    }
 }
